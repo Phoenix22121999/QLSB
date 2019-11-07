116ba4507fdf51b89ab8f47922e196768813ec57
Phoenix22121999 <Phoenix>
2019-11-07 15:48:09 +0700

    Create Main.Designer.cs

---
+++ WindowsFormsApp1/Main.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace QLSB
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.San1 = new System.Windows.Forms.Button();
+            this.bangsan = new System.Windows.Forms.FlowLayoutPanel();
+            this.San2 = new System.Windows.Forms.Button();
+            this.San3 = new System.Windows.Forms.Button();
+            this.San4 = new System.Windows.Forms.Button();
+            this.San5 = new System.Windows.Forms.Button();
+            this.San6 = new System.Windows.Forms.Button();
+            this.San7 = new System.Windows.Forms.Button();
+            this.San8 = new System.Windows.Forms.Button();
+            this.San9 = new System.Windows.Forms.Button();
+            this.themsan = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewYard = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.bangsan.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewYard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // San1
+            // 
+            this.San1.Location = new System.Drawing.Point(6, 6);
+            this.San1.Margin = new System.Windows.Forms.Padding(6);
+            this.San1.Name = "San1";
+            this.San1.Size = new System.Drawing.Size(75, 23);
+            this.San1.TabIndex = 0;
+            this.San1.Text = "Sân 1";
+            this.San1.UseVisualStyleBackColor = true;
+            this.San1.Click += new System.EventHandler(this.San1_Click);
+            // 
+            // bangsan
+            // 
+            this.bangsan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bangsan.Controls.Add(this.San1);
+            this.bangsan.Controls.Add(this.San2);
+            this.bangsan.Controls.Add(this.San3);
+            this.bangsan.Controls.Add(this.San4);
+            this.bangsan.Controls.Add(this.San5);
+            this.bangsan.Controls.Add(this.San6);
+            this.bangsan.Controls.Add(this.San7);
+            this.bangsan.Controls.Add(this.San8);
+            this.bangsan.Controls.Add(this.San9);
+            this.bangsan.Location = new System.Drawing.Point(326, 18);
+            this.bangsan.Name = "bangsan";
+            this.bangsan.Size = new System.Drawing.Size(90, 334);
+            this.bangsan.TabIndex = 1;
+            // 
+            // San2
+            // 
+            this.San2.Location = new System.Drawing.Point(6, 41);
+            this.San2.Margin = new System.Windows.Forms.Padding(6);
+            this.San2.Name = "San2";
+            this.San2.Size = new System.Drawing.Size(75, 23);
+            this.San2.TabIndex = 1;
+            this.San2.Text = "Sân2";
+            this.San2.UseVisualStyleBackColor = true;
+            // 
+            // San3
+            // 
+            this.San3.Location = new System.Drawing.Point(6, 76);
+            this.San3.Margin = new System.Windows.Forms.Padding(6);
+            this.San3.Name = "San3";
+            this.San3.Size = new System.Drawing.Size(75, 23);
+            this.San3.TabIndex = 2;
+            this.San3.Text = "Sân 3";
+            this.San3.UseVisualStyleBackColor = true;
+            // 
+            // San4
+            // 
+            this.San4.Location = new System.Drawing.Point(6, 111);
+            this.San4.Margin = new System.Windows.Forms.Padding(6);
+            this.San4.Name = "San4";
+            this.San4.Size = new System.Drawing.Size(75, 23);
+            this.San4.TabIndex = 3;
+            this.San4.Text = "Sân 4";
+            this.San4.UseVisualStyleBackColor = true;
+            // 
+            // San5
+            // 
+            this.San5.Location = new System.Drawing.Point(6, 146);
+            this.San5.Margin = new System.Windows.Forms.Padding(6);
+            this.San5.Name = "San5";
+            this.San5.Size = new System.Drawing.Size(75, 23);
+            this.San5.TabIndex = 4;
+            this.San5.Text = "Sân 5";
+            this.San5.UseVisualStyleBackColor = true;
+            // 
+            // San6
+            // 
+            this.San6.Location = new System.Drawing.Point(6, 181);
+            this.San6.Margin = new System.Windows.Forms.Padding(6);
+            this.San6.Name = "San6";
+            this.San6.Size = new System.Drawing.Size(75, 23);
+            this.San6.TabIndex = 5;
+            this.San6.Text = "Sân 6";
+            this.San6.UseVisualStyleBackColor = true;
+            // 
+            // San7
+            // 
+            this.San7.Location = new System.Drawing.Point(6, 216);
+            this.San7.Margin = new System.Windows.Forms.Padding(6);
+            this.San7.Name = "San7";
+            this.San7.Size = new System.Drawing.Size(75, 23);
+            this.San7.TabIndex = 6;
+            this.San7.Text = "Sân 7";
+            this.San7.UseVisualStyleBackColor = true;
+            // 
+            // San8
+            // 
+            this.San8.Location = new System.Drawing.Point(6, 251);
+            this.San8.Margin = new System.Windows.Forms.Padding(6);
+            this.San8.Name = "San8";
+            this.San8.Size = new System.Drawing.Size(75, 23);
+            this.San8.TabIndex = 5;
+            this.San8.Text = "Sân 8";
+            this.San8.UseVisualStyleBackColor = true;
+            // 
+            // San9
+            // 
+            this.San9.Location = new System.Drawing.Point(6, 286);
+            this.San9.Margin = new System.Windows.Forms.Padding(6);
+            this.San9.Name = "San9";
+            this.San9.Size = new System.Drawing.Size(75, 23);
+            this.San9.TabIndex = 7;
+            this.San9.Text = "Sân 9";
+            this.San9.UseVisualStyleBackColor = true;
+            // 
+            // themsan
+            // 
+            this.themsan.Location = new System.Drawing.Point(93, 129);
+            this.themsan.Name = "themsan";
+            this.themsan.Size = new System.Drawing.Size(85, 23);
+            this.themsan.TabIndex = 2;
+            this.themsan.Text = "Thêm Sân";
+            this.themsan.UseVisualStyleBackColor = true;
+            this.themsan.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Đặt Lịch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(6, 18);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(107, 17);
+            this.labelName.TabIndex = 4;
+            this.labelName.Text = "Tên Nhân Viên:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelName);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(299, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // dataGridViewYard
+            // 
+            this.dataGridViewYard.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewYard.Location = new System.Drawing.Point(434, 78);
+            this.dataGridViewYard.Name = "dataGridViewYard";
+            this.dataGridViewYard.RowHeadersWidth = 51;
+            this.dataGridViewYard.RowTemplate.Height = 24;
+            this.dataGridViewYard.Size = new System.Drawing.Size(0, 0);
+            this.dataGridViewYard.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(184, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Đặt Nước";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Main
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(439, 347);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridViewYard);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.themsan);
+            this.Controls.Add(this.bangsan);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản Lý Sân Bóng Mini";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.bangsan.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewYard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button San1;
+        private System.Windows.Forms.FlowLayoutPanel bangsan;
+        private System.Windows.Forms.Button themsan;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button San2;
+        private System.Windows.Forms.Button San3;
+        private System.Windows.Forms.Button San4;
+        private System.Windows.Forms.Button San5;
+        private System.Windows.Forms.Button San6;
+        private System.Windows.Forms.Button San7;
+        private System.Windows.Forms.Button San8;
+        private System.Windows.Forms.Button San9;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridViewYard;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
