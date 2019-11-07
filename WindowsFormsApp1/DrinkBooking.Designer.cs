d8b9e3ec08200aaf53365ec91e722ce5791eda60
Phoenix22121999 <Phoenix>
2019-11-07 15:47:54 +0700

    Create DrinkBooking.Designer.cs

---
+++ WindowsFormsApp1/DrinkBooking.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace QLSB
+{
+    partial class DrinkBooking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxDrink = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.waterText = new System.Windows.Forms.TextBox();
+            this.stingText = new System.Windows.Forms.TextBox();
+            this.iceTeaText = new System.Windows.Forms.TextBox();
+            this.redBullText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxYard = new System.Windows.Forms.ComboBox();
+            this.DrinkBook = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.comboBoxHour = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridViewDrinkBooking = new System.Windows.Forms.DataGridView();
+            this.groupBoxDrink.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDrinkBooking)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxDrink
+            // 
+            this.groupBoxDrink.Controls.Add(this.label8);
+            this.groupBoxDrink.Controls.Add(this.label7);
+            this.groupBoxDrink.Controls.Add(this.label6);
+            this.groupBoxDrink.Controls.Add(this.label5);
+            this.groupBoxDrink.Controls.Add(this.waterText);
+            this.groupBoxDrink.Controls.Add(this.stingText);
+            this.groupBoxDrink.Controls.Add(this.iceTeaText);
+            this.groupBoxDrink.Controls.Add(this.redBullText);
+            this.groupBoxDrink.Controls.Add(this.label3);
+            this.groupBoxDrink.Controls.Add(this.label2);
+            this.groupBoxDrink.Controls.Add(this.label1);
+            this.groupBoxDrink.Controls.Add(this.label4);
+            this.groupBoxDrink.Location = new System.Drawing.Point(12, 156);
+            this.groupBoxDrink.Name = "groupBoxDrink";
+            this.groupBoxDrink.Size = new System.Drawing.Size(288, 237);
+            this.groupBoxDrink.TabIndex = 0;
+            this.groupBoxDrink.TabStop = false;
+            this.groupBoxDrink.Text = "Đồ Uống";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(151, 191);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Chai/12.000VND";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(151, 142);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(102, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Ca/10.000VND";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(151, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Lon/12.000VND";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(151, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Chai/10.000VND";
+            // 
+            // waterText
+            // 
+            this.waterText.Location = new System.Drawing.Point(83, 42);
+            this.waterText.Name = "waterText";
+            this.waterText.Size = new System.Drawing.Size(62, 22);
+            this.waterText.TabIndex = 8;
+            // 
+            // stingText
+            // 
+            this.stingText.Location = new System.Drawing.Point(83, 188);
+            this.stingText.Name = "stingText";
+            this.stingText.Size = new System.Drawing.Size(62, 22);
+            this.stingText.TabIndex = 7;
+            // 
+            // iceTeaText
+            // 
+            this.iceTeaText.Location = new System.Drawing.Point(83, 139);
+            this.iceTeaText.Name = "iceTeaText";
+            this.iceTeaText.Size = new System.Drawing.Size(62, 22);
+            this.iceTeaText.TabIndex = 6;
+            // 
+            // redBullText
+            // 
+            this.redBullText.Location = new System.Drawing.Point(83, 91);
+            this.redBullText.Name = "redBullText";
+            this.redBullText.Size = new System.Drawing.Size(62, 22);
+            this.redBullText.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 181);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(5);
+            this.label3.Size = new System.Drawing.Size(50, 27);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Sting";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(5);
+            this.label2.Size = new System.Drawing.Size(78, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nước Lọc";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 132);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(5);
+            this.label1.Size = new System.Drawing.Size(62, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Trà Đá";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 86);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(5);
+            this.label4.Size = new System.Drawing.Size(71, 27);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Red Bull";
+            // 
+            // comboBoxYard
+            // 
+            this.comboBoxYard.FormattingEnabled = true;
+            this.comboBoxYard.Location = new System.Drawing.Point(95, 13);
+            this.comboBoxYard.Name = "comboBoxYard";
+            this.comboBoxYard.Size = new System.Drawing.Size(205, 24);
+            this.comboBoxYard.TabIndex = 1;
+            this.comboBoxYard.TextUpdate += new System.EventHandler(this.comboBoxYard_SelectedIndexChanged);
+            // 
+            // DrinkBook
+            // 
+            this.DrinkBook.Location = new System.Drawing.Point(199, 399);
+            this.DrinkBook.Name = "DrinkBook";
+            this.DrinkBook.Size = new System.Drawing.Size(101, 32);
+            this.DrinkBook.TabIndex = 2;
+            this.DrinkBook.Text = "Đặt";
+            this.DrinkBook.UseVisualStyleBackColor = true;
+            this.DrinkBook.Click += new System.EventHandler(this.DrinkBook_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(17, 13);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 20);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Sân:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(17, 60);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 20);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "Giờ:";
+            // 
+            // comboBoxHour
+            // 
+            this.comboBoxHour.FormattingEnabled = true;
+            this.comboBoxHour.Location = new System.Drawing.Point(95, 60);
+            this.comboBoxHour.Name = "comboBoxHour";
+            this.comboBoxHour.Size = new System.Drawing.Size(205, 24);
+            this.comboBoxHour.TabIndex = 5;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(17, 108);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(52, 20);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "Ngày:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(95, 108);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(205, 22);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // dataGridViewDrinkBooking
+            // 
+            this.dataGridViewDrinkBooking.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDrinkBooking.Location = new System.Drawing.Point(50, 51);
+            this.dataGridViewDrinkBooking.Name = "dataGridViewDrinkBooking";
+            this.dataGridViewDrinkBooking.RowHeadersWidth = 51;
+            this.dataGridViewDrinkBooking.RowTemplate.Height = 24;
+            this.dataGridViewDrinkBooking.Size = new System.Drawing.Size(0, 0);
+            this.dataGridViewDrinkBooking.TabIndex = 8;
+            // 
+            // DrinkBooking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 443);
+            this.Controls.Add(this.dataGridViewDrinkBooking);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.comboBoxHour);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.DrinkBook);
+            this.Controls.Add(this.comboBoxYard);
+            this.Controls.Add(this.groupBoxDrink);
+            this.MaximizeBox = false;
+            this.Name = "DrinkBooking";
+            this.Text = "Đặt nước";
+            this.Load += new System.EventHandler(this.DrinkBooking_Load);
+            this.groupBoxDrink.ResumeLayout(false);
+            this.groupBoxDrink.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDrinkBooking)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBoxDrink;
+        private System.Windows.Forms.ComboBox comboBoxYard;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox waterText;
+        private System.Windows.Forms.TextBox stingText;
+        private System.Windows.Forms.TextBox iceTeaText;
+        private System.Windows.Forms.TextBox redBullText;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button DrinkBook;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBoxHour;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridViewDrinkBooking;
+    }
+}
