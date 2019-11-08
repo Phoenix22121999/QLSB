07b23d0b16c436cf1f035dde3d8b2539c961d6ab
Phoenix22121999 <Phoenix>
2019-11-08 08:45:22 +0700

    Update Cons.cs

--- WindowsFormsApp1/Cons.cs
+++ WindowsFormsApp1/Cons.cs
@@ -16,7 +16,6 @@ namespace QLSB
         //public static String sqlLink = @"Data Source=DESKTOP-0SVJ4PH;Initial Catalog=QLSB;Integrated Security=True";
         public static String[] gio = new string[] { "6h-7h", "7h-8h", "8h-9h", "9h-10h", "10h-11h", "13h-14h", "14h-15h", "15h-16h", "16h-17h", "17h-18h", "18h-19h", "19h-20h", "20h-21h" };
         public static String[] san = new string[] { "Sân 1", "Sân 2", "Sân 3", "Sân 4", "Sân 5", "Sân 6", "Sân 7", "Sân 8", "Sân 9" };
-        public static String sqlLink = @"Data Source = localhost; Initial Catalog = QLSB; Integrated Security = True";
         public static String sqlLink = @"Data Source=DESKTOP\SQLEXPRESS;Initial Catalog=QLSB;Integrated Security=True";
     }
 }
